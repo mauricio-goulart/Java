af73c05a5b4f58c477aab98f3e7bb0d5f5e332ad
mauricio <mauricioazevedogolf@gmail.com>
2025-11-17 23:34:50 -0300

    Att

--- JavaProject/src/App.java
+++ JavaProject/src/App.java
@@ -18,7 +18,7 @@ public class App {
 
         //- Operadores logicos
         
-        System.out.printf("Voce é maior de idade, digite a sua idade: ");
+        System.out.println("Voce é maior de idade, digite a sua idade: ");
         var result = scanner.nextInt();
         var isRight = (result != 0 && result > 18);  // > < =! >= <=
 
@@ -29,5 +29,55 @@ public class App {
         var n1 = 2;
         var n2 = 4;
         System.out.println(Math.pow(n1, n2));
+
+        //- Condicional if else
+
+        System.out.println("Qual seu nome: ");
+        var name1 = scanner.next();
+
+        if (name1.equals("mauricio")) {
+
+            System.out.println("Great nome");
+
+        }else {
+
+            System.out.println("Fail nome");
+        }
+
+        //- Condicional Switch case
+
+        var caso = scanner.nextInt();
+
+        switch (caso) {
+            case 1:
+                System.out.println("Numero 1");
+                break;
+
+            case 2:
+                System.out.println("Numero 2");
+                break;
+
+            default:
+                System.out.println("Error");
+        }
+
+        //- Estrutura de repetição for
+
+        for (var i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //- Estrutura de repetição while; do while
+
+        var name2 = "";
+        while (!name.equalsIgnoreCase("exit")) {
+
+            System.out.println("Digite EXIT: ");
+            name2 = scanner.next();
+
+        }
+
+
+
     }
 }
